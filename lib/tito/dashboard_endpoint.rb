2fe11931297127f19edbd79c3f5316027741d679
Alex Thomas <alexthomas@Alexs-MacBook-Pro.local>
2017-11-17 11:40:41 +0000

    Bug fix: dashboard extend ClassMethods

--- lib/tito/dashboard_endpoint.rb
+++ lib/tito/dashboard_endpoint.rb
@@ -1,16 +1,15 @@
 module Tito
   module DashboardEndpoint
-    base.extend ClassMethods
     
     def self.included(base)
-
+      base.extend ClassMethods
       def initialize(args)
         super(args)
       end
     
     end
     
-    def ClassMethods
+    module ClassMethods
       
       def client
         @@client ||= Tito::http.new({host_name: 'https://dashboard.tito.io', base_endpoint: "#{Tito::config.host_name}/#{Tito::config.account}/"})
