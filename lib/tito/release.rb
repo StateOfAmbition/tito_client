b69825a11bd4ca637e246f141931586687f382ed
Alex Thomas <alexthomas@Alexs-MacBook-Pro.local>
2017-12-03 18:04:34 +0000

    feature: allow tickets to be paid by invoice

--- lib/tito/release.rb
+++ lib/tito/release.rb
@@ -4,7 +4,7 @@ module Tito
 
     class << self
       def attributes
-        [:title, :description, :enable_super_combo_summary, :has_success_message, :max_tickets_per_person,
+        [:title, :description, :enable_super_combo_summary, :has_success_message, :invoice, :max_tickets_per_person,
           :min_tickets_per_person, :not_a_ticket, :position, :price, :quantity, :question_ids, :request_company_name,
           :request_vat_number, :require_billing_address, :require_email, :require_name, :secret, :slug, :state, :start_at,
           :end_at, :success_message_text, :waiting_list_enabled_during_sold_out, :archived, :default_quantity]
