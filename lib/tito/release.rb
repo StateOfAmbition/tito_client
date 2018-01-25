d58b5c2c7c32130c758bb283f66e196bf8587141
Alex Thomas <alexthomas@Alexs-MacBook-Pro.local>
2018-01-25 15:41:22 +0000

    feature: void releases

--- lib/tito/release.rb
+++ lib/tito/release.rb
@@ -3,6 +3,19 @@ module Tito
     include Eventable, DashboardEndpoint
 
     class << self
+
+      def void(event_slug, id)
+        self.find(event_slug, id, ["tickets"]).included.each do |t|
+          Tito::Ticket.void(event_slug, t.id) unless t.state == "void"
+        end
+      end
+
+      def un_void(event_slug, id)
+        self.find(event_slug, id, ["tickets"]).included.each do |t|
+          Tito::Ticket.un_void(event_slug, t.id) if t.state == "void"
+        end
+      end
+
       def attributes
         [:title, :description, :enable_super_combo_summary, :has_success_message, :invoice, :max_tickets_per_person, :metadata,
           :min_tickets_per_person, :not_a_ticket, :position, :price, :quantity, :question_ids, :request_company_name,
