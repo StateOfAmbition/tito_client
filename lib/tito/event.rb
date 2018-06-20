6fbfd3c9ea920247ee22603dfc21c07a3c65dd72
Alex Thomas <alexthomas@Alexs-MacBook-Pro-2.local>
2018-06-20 15:14:17 +0100

    Add class destroy method for events

--- lib/tito/event.rb
+++ lib/tito/event.rb
@@ -21,6 +21,10 @@ module Tito
         client.get(resource_type).resources
       end
 
+      def destroy(id)
+        client.destroy(id)
+      end
+
       def attributes
         [:title, :description, :slug, :currency, :start_date, :end_date, :location, :test_mode, :private, :live, :logo_url, :banner_url]
       end
