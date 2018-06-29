cfeaaa6f49e2a7794c4862d22443cdbffe468c00
Alex Thomas <alexthomas@Alexs-MacBook-Pro-2.local>
2018-06-29 16:15:27 +0100

    Add date & time attributes to activity

--- lib/tito/activity.rb
+++ lib/tito/activity.rb
@@ -4,7 +4,7 @@ module Tito
 
     class << self
       def attributes
-        [:name, :capacity, :private, :release_ids]
+        [:name, :capacity, :private, :release_ids, :date, :start_at, :end_at]
       end
     end
 
