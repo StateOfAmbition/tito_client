ffe9f724233ad9077f144036a1a768ab66694481
Alex Thomas <alex.c.thomas@gmail.com>
2019-02-20 16:25:24 +0000

    bug fix: missing amount off for discount codes

--- lib/tito/discount_code.rb
+++ lib/tito/discount_code.rb
@@ -5,7 +5,7 @@ module Tito
     class << self
 
       def attributes
-        [:code, :value, :type, :quantity, :min_quantity_per_release,
+        [:amount_off, :code, :value, :type, :quantity, :min_quantity_per_release,
           :max_quantity_per_release, :start_at, :end_at, :release_ids,
           :only_show_attached, :reveal_secret, :source_id, :disable_for_degressive
         ]
