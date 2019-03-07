8dade83ea1cde6d164061a8d97e2afc99e298bd1
Alex Thomas <alex.c.thomas@gmail.com>
2019-03-07 22:02:54 +0000

    add pricing_type to release

--- lib/tito/release.rb
+++ lib/tito/release.rb
@@ -18,7 +18,7 @@ module Tito
 
       def attributes
         [:title, :description, :enable_super_combo_summary, :has_success_message, :invoice, :max_tickets_per_person, :metadata,
-          :min_tickets_per_person, :not_a_ticket, :position, :price, :price_degressive, :quantity, :question_ids, :request_company_name,
+          :min_tickets_per_person, :not_a_ticket, :position, :price, :price_degressive, :pricing_type, :quantity, :question_ids, :request_company_name,
           :request_vat_number, :require_billing_address, :require_email, :require_name, :secret, :slug, :state, :start_at,
           :end_at, :success_message_text, :waiting_list_enabled_during_sold_out, :archived, :default_quantity]
       end
