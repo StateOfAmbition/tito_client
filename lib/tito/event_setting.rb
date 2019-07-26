550f9e9cddd6055bda8fadfeaeb3662a34dfb9c9
Alex Thomas <alex.c.thomas@gmail.com>
2019-07-26 16:31:33 +0100

    feature: add google analytics id to event_settings

--- lib/tito/event_setting.rb
+++ lib/tito/event_setting.rb
@@ -17,7 +17,7 @@ module Tito
          :custom_background_color, :custom_complete_ticket_label, :custom_email_signature, :custom_registration_confirmation_subject,
          :custom_registration_unavailable_message, :custom_ticket_added_subject, :custom_ticket_gifted_subject, :custom_ticket_message,
          :custom_ticket_updated_subject, :custom_view_ticket_label, :default_billing_country, :display_date, :display_title, :email_address,
-         :email_image_url, :facebook_share_message, :invoice_change_cutoff_days, :invoice_extra, :invoice_number_format, :invoice_prefix, :invoice_tax_details,
+         :email_image_url, :facebook_share_message, :google_analytics_property_id, :invoice_change_cutoff_days, :invoice_extra, :invoice_number_format, :invoice_prefix, :invoice_tax_details,
          :lanyrd_url, :logo_height, :logo_width, :map_latitude, :map_longitude, :map_link, :map_zoomlevel, :max_tickets_per_person, :min_tickets_per_person, :organization_address,
          :organisation_name, :passbook_icon_url, :payment_instructions, :receipt_footer_message, :register_button_label, :register_interest_form_label, :organization_name, :passbook_icon_url,
          :receipt_footer_message, :register_button_label, :register_interest_form_label, :requires_billing_address, :requires_company_name, :requires_country, :show_additional_info, :show_banner,
--- lib/tito/version.rb
+++ lib/tito/version.rb
@@ -1,3 +1,3 @@
 module TitoClient
-  VERSION = '1.5'
+  VERSION = '1.6'
 end
