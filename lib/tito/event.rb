4aaed5b2555ddaf322cf4cbac52742b3736cf9e3
Alex Thomas <alex.c.thomas@gmail.com>
2019-07-26 17:09:45 +0100

    feature: add google analytics id attribute to event

--- lib/tito/event.rb
+++ lib/tito/event.rb
@@ -30,7 +30,9 @@ module Tito
       end
 
       def attributes
-        [:title, :description, :slug, :currency, :credit_card_payment_option_id, :start_date, :end_date, :location, :test_mode, :private, :live, :logo_url, :banner_url]
+        [:title, :description, :slug, :currency, :credit_card_payment_option_id,
+          :start_date, :end_date, :location, :test_mode, :private, :live,
+          :logo_url, :banner_url, :google_analytics_property_id]
       end
     end
   end
