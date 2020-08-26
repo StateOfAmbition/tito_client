ac02e44154d53ad51ac35d4a5eb05200e1d68d5f
Alex Thomas <alex.c.thomas@gmail.com>
2020-08-26 10:55:24 +0100

    feature: update settings endpoints

--- lib/tito/setting/services.rb
+++ lib/tito/setting/services.rb
@@ -11,8 +11,8 @@ module Tito
         def attributes
           [
             :after_registration_complete_url, :after_registration_redirect_after_questions,
-            :after_registration_closed_url, :show_sharing_links, :twitter_share_message_en,
-            :social_sharing_title, :social_sharing_description, :metadata
+            :after_registration_closed_url, :google_analytics_property_id, :google_analytics_domains,
+            :show_sharing_links, :twitter_share_message_en, :social_sharing_title, :social_sharing_description, :metadata
           ]
         end
       end
--- lib/tito/version.rb
+++ lib/tito/version.rb
@@ -1,3 +1,3 @@
 module TitoClient
-  VERSION = '2.3.0'
+  VERSION = '2.4'
 end
